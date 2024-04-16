94f5a1baf635da6df1ddfdaa0c3fe95fdb015f81
Hazopspartan <167109299+Hazopspartan@users.noreply.github.com>
2024-04-16 16:06:57 -0700

    Generic GameMode.hpp

--- FiveLetterMode.hpp
+++ FiveLetterMode.hpp
@@ -7,13 +7,15 @@
 #include <sstream>
 #include <chrono>
 
-class FiveLM {
+class GameMode {
 private:
-	int cols = 5;
+	int cols;
 
-	std::ifstream fiveStream;
+	std::ifstream libStream;
 protected:
+
 	int lives = 3;
+	int rows = 6;
 
 	// generates the blank board with the correct # of dark grey boxes derived from row & cols count
 	void generateBoard(int rows, int cols);
@@ -22,7 +24,7 @@ protected:
 	void chooseMysteryWord(std::string& mysteryWord);
 
 	// prompts user for a guess and stores it for later use;
-	void getPlayerGuess(std::string &guess);
+	void getPlayerGuess(std::string& guess);
 
 	// compares the guess to the word, and determines what letters are correct, which ones are in the word but not in the right spot, and which aren't in the word.
 	void compareGuessToWord(const std::string guess, const std::string word);
@@ -34,37 +36,6 @@ protected:
 	void displayStats();
 public:
 
-	int rows = 6;
-
 	void runGame();
+
 };
-
-#include "LetterGameModes.hpp"
-
-void FiveLM::generateBoard(int rows, int cols)
-{
-}
-
-void FiveLM::chooseMysteryWord(std::string& mysteryWord)
-{
-}
-
-void FiveLM::getPlayerGuess(std::string& guess)
-{
-}
-
-void FiveLM::compareGuessToWord(const std::string guess, const std::string word)
-{
-}
-
-void FiveLM::updateBox(int boxType, int row, int cols)
-{
-}
-
-void FiveLM::displayStats()
-{
-}
-
-void FiveLM::runGame()
-{
-}
