194169776c0f982da52387faf5a286b3e7774181
Hazopspartan <167109299+Hazopspartan@users.noreply.github.com>
2024-04-16 16:07:29 -0700

    GameMode.cpp

---
+++ GameMode.cpp
@@ -0,0 +1,29 @@
+#include "GameMode.hpp"
+
+void GameMode::generateBoard(int rows, int cols)
+{
+}
+
+void GameMode::chooseMysteryWord(std::string& mysteryWord)
+{
+}
+
+void GameMode::getPlayerGuess(std::string& guess)
+{
+}
+
+void GameMode::compareGuessToWord(const std::string guess, const std::string word)
+{
+}
+
+void GameMode::updateBox(int boxType, int row, int cols)
+{
+}
+
+void GameMode::displayStats()
+{
+}
+
+void GameMode::runGame()
+{
+}
