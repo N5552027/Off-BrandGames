d35e4f0d820154f636cffd722c2a4c503448f864
N Mike <143025594+N5552027@users.noreply.github.com>
2024-04-18 10:43:44 -0700

    Update guessValidifierCode.cpp

--- guessValidifierCode.cpp
+++ guessValidifierCode.cpp
@@ -1,26 +1,23 @@
-void tempGuessValidifierCode(std::string playerGuess, std::string secretWord) {
-
+void tempGuessValidifierCode(std::string playerGuess, std::string secretWord) 
+{
 	for (std::string::iterator playerGuessIterator = playerGuess.begin, int guessLetterIndex = 0; playerGuessIterator != playerGuess.end; playerGuessIterator++, guessLetterIndex++) { // TRAVERSE THE PLAYER GUESS
 		for (std::string::iterator secretWordIterator = secretWord.begin, int secretLetterIndex = 0; secretWordIterator != secretWord.end; secretWordIterator++, guessLetterIndex++) { // TRAVERSE THE MYSTERY WORD
 			
-			if (*playerGuessIterator == *secretWordIterator) { // LETTER IS FOUND
-				
-				if (guessLetterIndex == secretLetterIndex) { // IN GAME CASE = GREEN
-
+			if (*playerGuessIterator == *secretWordIterator) // LETTER FOUND
+			{ 
+				if (guessLetterIndex == secretLetterIndex) {  // INDEX MATCH (CHANGE TO GREEN)
 					std::cout << "The letter " << *playerGuessIterator << " is at index " << guessLetterIndex << std::endl
 						<< "The Mystery Word also has the letter" << *secretWordIterator << "at index " << secretLetterIndex << std::endl
 						<< "It's a match!" << std::endl;
-					
 				}
-				else {	// IN GAME CASE: CHANGE YELLOW
-
+				else {	// INDEXES DON'T MATCH (CHANGE TO YELLOW)
 					std::cout << "The letter" << *playerGuessIterator << "is at index " << guessLetterIndex << std::endl
 						<< "The mystery Word also has the letter" << *secretWordIterator << "at index" << secretLetterIndex << std::endl
 						<< "It's not a match but at least the letter is there!" << std::endl;
-
 				}
 			}
-			else { // In Game Case: Change gray
+			else // LETTER IS NOT FOUND: CHANGE TILE TO GRAY
+			{
 			
 				std::cout << "THe letter" << *playerGuessIterator << "ist at index " << guessLetterIndex << std::endl
 					<< "The mystery word does not contain the letter" << *playerGuessIterator << "in any index." << std::endl
@@ -28,4 +25,5 @@ void tempGuessValidifierCode(std::string playerGuess, std::string secretWord) {
 			}
 		
 		}	
+	}
 }
