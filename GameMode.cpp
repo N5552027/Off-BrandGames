f2eedf2874a0ba097205ce414052aa2dfdcad9a7
Hazopspartan <167109299+Hazopspartan@users.noreply.github.com>
2024-04-17 19:30:18 -0700

    GameMode.cpp

--- GameMode.cpp
+++ GameMode.cpp
@@ -8,8 +8,21 @@ void GameMode::chooseMysteryWord(std::string& mysteryWord)
 {
 }
 
-void GameMode::getPlayerGuess(std::string& guess)
+std::string GameMode::getPlayerGuess(int wordLength)
 {
+	std::string guess;
+
+	while (true) {
+		std::cout << "Enter a " << wordLength << "-letter word: ";
+		std::cin >> guess;
+		if (guess.length() == wordLength) {
+			break;
+		}
+		else {
+			std::cout << "Invalid input. Please enter a word that is " << wordLength << " letters long." << std::endl;
+		}
+	}
+	return guess;
 }
 
 void GameMode::compareGuessToWord(const std::string guess, const std::string word)
