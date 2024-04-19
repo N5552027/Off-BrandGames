cab5096e50e7177ac30a418a51e91882f6fc1ce8
rubsacc <62792386+rubsacc@users.noreply.github.com>
2024-04-18 17:20:07 -0700

    Update GameMode.cpp

--- GameMode.cpp
+++ GameMode.cpp
@@ -6,8 +6,35 @@ void GameMode::generateBoard(int rows, int cols)
 
 void GameMode::chooseMysteryWord(std::string& mysteryWord)
 {
-}
+	std::ifstream file("..."); // depending on file
 
+	if (!file.is_open()) 
+	{
+        	std::cerr << "Error: Unable to open file.\n";
+        	return;
+	}
+
+	// read words from file into a vector
+	std::vector<std::string> words;
+	std::string word;
+	while(file >> word)
+	{
+		words.push_back(word);
+	}
+
+	file.close();
+
+	if (words.empty()) 
+	{
+        	std::cerr << "Error: No words found in file.\n";
+        	return;
+	}
+
+	// random generation of word from vector 
+	srand(static_cast<unsigned int>(time)nullptr))); // seed the rng
+	int randomIndex = rand() % words.size();
+	mysteryWord = words[randomIndex];
+}
 std::string GameMode::getPlayerGuess(int wordLength)
 {
 	std::string guess;
