04efec0486840eb313d66f547faff450a8ac6722
Hazopspartan <167109299+Hazopspartan@users.noreply.github.com>
2024-04-18 18:36:20 -0700

    Update guessValidifierCode.cpp

--- guessValidifierCode.cpp
+++ guessValidifierCode.cpp
@@ -1,33 +1,47 @@
 void compareGuess(const std::string& playerGuess) {
-	secretGuessValidifierCode(playerGuess, this->secretWord);
+	secretGuessValidifierCode(playerGuess, secretWord);
 }
 
 void secretGuessValidifierCode(std::string playerGuess, std::string secretWord) 
 {
-	for (std::string::iterator playerGuessIterator = playerGuess.begin, int guessLetterIndex = 0; playerGuessIterator != playerGuess.end; playerGuessIterator++, guessLetterIndex++) { // TRAVERSE THE PLAYER GUESS
-		for (std::string::iterator secretWordIterator = secretWord.begin, int secretLetterIndex = 0; secretWordIterator != secretWord.end; secretWordIterator++, guessLetterIndex++) { // TRAVERSE THE MYSTERY WORD
+	std::string::iterator playerGuessIterator,
+							secretWordIterator;
+	int guessLetterIndex,
+		secretLetterIndex;
+
+	bool letterPresence = false;
+
+		for (playerGuessIterator = playerGuess.begin(), guessLetterIndex = 0; playerGuessIterator != playerGuess.end(); playerGuessIterator++, guessLetterIndex++) { // TRAVERSE THE PLAYER GUESS\
 			
-			if (*playerGuessIterator == *secretWordIterator) // LETTER FOUND
-			{ 
-				if (guessLetterIndex == secretLetterIndex) {  // INDEX MATCH (CHANGE TO GREEN)
-					std::cout << "The letter " << *playerGuessIterator << " is at index " << guessLetterIndex << std::endl
-						<< "The Mystery Word also has the letter" << *secretWordIterator << "at index " << secretLetterIndex << std::endl
-						<< "It's a match!" << std::endl;
+			letterPresence = false;
+
+			for (secretWordIterator = secretWord.begin(), secretLetterIndex = 0; secretWordIterator != secretWord.end(); secretWordIterator++, secretLetterIndex++) { // TRAVERSE THE MYSTERY WORD
+
+
+				if (*playerGuessIterator == *secretWordIterator) // LETTER FOUND
+				{
+					letterPresence = true;
+
+					if (guessLetterIndex == secretLetterIndex) {  // INDEX MATCH (CHANGE TO GREEN)
+						std::cout << "The letter " << *playerGuessIterator << " is at index " << guessLetterIndex << std::endl
+							<< "The Mystery Word also has the letter " << *secretWordIterator << " at index " << secretLetterIndex << std::endl
+							<< " It's a match! " << std::endl;
+					}
+					else {	// INDEXES DON'T MATCH (CHANGE TO YELLOW)
+						std::cout << "The letter " << *playerGuessIterator << " is at index " << guessLetterIndex << std::endl
+							<< "The mystery Word also has the letter " << *secretWordIterator << " at index " << secretLetterIndex << std::endl
+							<< "It's not a match but at least the letter is there!" << std::endl;
+					}
 				}
-				else {	// INDEXES DON'T MATCH (CHANGE TO YELLOW)
-					std::cout << "The letter" << *playerGuessIterator << "is at index " << guessLetterIndex << std::endl
-						<< "The mystery Word also has the letter" << *secretWordIterator << "at index" << secretLetterIndex << std::endl
-						<< "It's not a match but at least the letter is there!" << std::endl;
-				}
+				
 			}
-			else // LETTER IS NOT FOUND: CHANGE TILE TO GRAY
-			{
-			
-				std::cout << "THe letter" << *playerGuessIterator << "ist at index " << guessLetterIndex << std::endl
-					<< "The mystery word does not contain the letter" << *playerGuessIterator << "in any index." << std::endl
+
+			if (letterPresence == false) {
+				std::cout << "The letter " << *playerGuessIterator << " is at index " << guessLetterIndex << std::endl
+					<< "The mystery word does not contain the letter " << *playerGuessIterator << " in any index. " << std::endl
 					<< "It's not in the word!" << std::endl;
 			}
+
 		
-		}	
-	}
+		}
 }
