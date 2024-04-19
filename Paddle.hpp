b53a23a1e01c7a4c5e060fb39d82b183c7247d5c
MaxUpton <145073163+MaxUpton@users.noreply.github.com>
2024-04-18 19:02:20 -0700

    Create Paddle.hpp

---
+++ Paddle.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class Box : public sf::RectangleShape
+{
+public:
+
+	Box(const sf::Vector2f& size, const sf::Vector2f& pos, const sf::Color& color) :sf::RectangleShape(size)
+	{
+
+		this->setPosition(pos);
+		this->setFillColor(color);
+
+
+	}
+
+	void setGreen();
+	void setYellow();
+
+
+private:
+
+
+
+};
+
+class Guess :public sf::Text
+{
+
+public:
+
+	Guess(const int& size, const sf::Vector2f& pos, const sf::Color& color, const sf::Font& font)
+	{
+
+		this->setCharacterSize(size);
+		this->setPosition(pos);
+		this->setFillColor(color);
+		this->setFont(font);
+		this->setString("a");
+	}
+
+
+
+};
