402f3c2942119f7b01de6d322c2d9d5a71b4354a
Hazopspartan <167109299+Hazopspartan@users.noreply.github.com>
2024-04-18 18:49:37 -0700

    Update GameMode.cpp

--- GameMode.cpp
+++ GameMode.cpp
@@ -4,56 +4,90 @@ void GameMode::generateBoard(int rows, int cols)
 {
 }
 
-void GameMode::chooseMysteryWord(std::string& mysteryWord)
+void GameMode::chooseMysteryWord()
 {
-	std::ifstream file("..."); // depending on file
+	mysteryWord = mysteryWordBank[rand() % mysteryWordBankSize];
+	std::cout << "Mystery Word = " << mysteryWord << std::endl;
 
-	if (!file.is_open()) 
-	{
-        	std::cerr << "Error: Unable to open file.\n";
-        	return;
-	}
-
-	// read words from file into a vector
-	std::vector<std::string> words;
-	std::string word;
-	while(file >> word)
-	{
-		words.push_back(word);
-	}
-
-	file.close();
-
-	if (words.empty()) 
-	{
-        	std::cerr << "Error: No words found in file.\n";
-        	return;
-	}
-
-	// random generation of word from vector 
-	srand(static_cast<unsigned int>(time)nullptr))); // seed the rng
-	int randomIndex = rand() % words.size();
-	mysteryWord = words[randomIndex];
 }
+
+void GameMode::openTXTFile()
+{
+	std::cout << "OPENING FILE STREAM" << std::endl;
+}
+
+
 std::string GameMode::getPlayerGuess(int wordLength)
 {
 	std::string guess;
 
-	while (true) {
+	while (1) {
 		std::cout << "Enter a " << wordLength << "-letter word: ";
 		std::cin >> guess;
 		if (guess.length() == wordLength) {
 			break;
 		}
 		else {
-			std::cout << "Invalid input. Please enter a word that is " << wordLength << " letters long." << std::endl;
+			system("cls");
 		}
 	}
 	return guess;
 }
 
-void GameMode::compareGuessToWord(const std::string guess, const std::string word)
+void GameMode::loadWordBank()
 {
+	while (!this->inFileLoad.eof()) { // END CONDITION: END OF TXT FILE
+		std::string temp;
+		std::getline(inFileLoad, temp); 
+		mysteryWordBank.push_back(temp); 
+		mysteryWordBankSize++;
+	}
+	std::cout << "WORD BANK LOADED!" << std::endl;
+	inFileLoad.close();
+}
+
+void GameMode::compareGuessToWord(std::string playerGuess, std::string secretWord)
+{
+	std::string::iterator playerGuessIterator,
+							secretWordIterator;
+	int guessLetterIndex,
+		secretLetterIndex;
+
+	bool letterPresence = false;
+
+		for (playerGuessIterator = playerGuess.begin(), guessLetterIndex = 0; playerGuessIterator != playerGuess.end(); playerGuessIterator++, guessLetterIndex++) { // TRAVERSE THE PLAYER GUESS\
+			
+			letterPresence = false;
+
+			for (secretWordIterator = secretWord.begin(), secretLetterIndex = 0; secretWordIterator != secretWord.end(); secretWordIterator++, secretLetterIndex++) { // TRAVERSE THE MYSTERY WORD
+
+
+				if (*playerGuessIterator == *secretWordIterator) // LETTER FOUND
+				{
+					letterPresence = true;
+
+					if (guessLetterIndex == secretLetterIndex) {  // INDEX MATCH (CHANGE TO GREEN)
+						std::cout << "The letter " << *playerGuessIterator << " is at index " << guessLetterIndex << std::endl
+							<< "The Mystery Word also has the letter " << *secretWordIterator << " at index " << secretLetterIndex << std::endl
+							<< " It's a match! " << std::endl;
+					}
+					else {	// INDEXES DON'T MATCH (CHANGE TO YELLOW)
+						std::cout << "The letter " << *playerGuessIterator << " is at index " << guessLetterIndex << std::endl
+							<< "The mystery Word also has the letter " << *secretWordIterator << " at index " << secretLetterIndex << std::endl
+							<< "It's not a match but at least the letter is there!" << std::endl;
+					}
+				}
+				
+			}
+
+			if (letterPresence == false) {
+				std::cout << "The letter " << *playerGuessIterator << " is at index " << guessLetterIndex << std::endl
+					<< "The mystery word does not contain the letter " << *playerGuessIterator << " in any index. " << std::endl
+					<< "It's not in the word!" << std::endl;
+			}
+
+		
+		}
 }
 
 void GameMode::updateBox(int boxType, int row, int cols)
@@ -66,4 +100,5 @@ void GameMode::displayStats()
 
 void GameMode::runGame()
 {
+	// ALL GAME MODES ARE DEFINED BASED ON THEIR OVERRIDEN FUNCTION. NO CODE HAPPENS HERE
 }
