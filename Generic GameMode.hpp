1a42c0e65db5bd3622bbdf09c68eb2502f7fc855
Hazopspartan <167109299+Hazopspartan@users.noreply.github.com>
2024-04-18 18:47:37 -0700

    Update Generic GameMode.hpp

--- Generic GameMode.hpp
+++ Generic GameMode.hpp
@@ -1,41 +1,46 @@
 #pragma once
+#include "Header.hpp"
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cctype>
-#include <sstream>
-#include <chrono>
 
 class GameMode {
-private:
-	int cols;
-
-	std::ifstream libStream;
 protected:
 
+// ===== DATA MEMBERS =====
 	int lives = 3;
 	int rows = 6;
+	int mysteryWordBankSize;
+
+	std::ifstream inFileLoad;
+	std::vector<std::string> mysteryWordBank;
+	std::string mysteryWord;
+	std::string playerGuess;
+
+// ===== FUNCTIONS =====
+	// LOAD WORD BANK FUNCTIONS 
+	virtual void openTXTFile();
+	void loadWordBank();
 
 	// generates the blank board with the correct # of dark grey boxes derived from row & cols count
 	void generateBoard(int rows, int cols);
 
 	// takes a random word from the dedicated mode file, and designates it as the word
-	void chooseMysteryWord(std::string& mysteryWord);
-
+	void chooseMysteryWord();
+	
 	// prompts user for a guess and stores it for later use;
-	std::string getPlayerGuess(int wordLength);
+	std::string getPlayerGuess(int wordLegth);
 
 	// compares the guess to the word, and determines what letters are correct, which ones are in the word but not in the right spot, and which aren't in the word.
-	void compareGuessToWord(const std::string guess, const std::string word);
+	void compareGuessToWord(std::string playerGuess, std::string secretWord);
 
 	// updates the current box with the type it needs to be
 	void updateBox(int boxType, int row, int cols);
 
 	// displays the different stats of the player
 	void displayStats();
+
+
 public:
 
-	void runGame();
+	virtual void runGame();
 
 };
