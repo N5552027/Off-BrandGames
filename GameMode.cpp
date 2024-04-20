bab9a75dedcdb2779e9156766f435c5b9e2991b5
N Mike <143025594+N5552027@users.noreply.github.com>
2024-04-20 00:06:42 -0700

    Update GameMode.cpp

--- GameMode.cpp
+++ GameMode.cpp
@@ -98,6 +98,30 @@ void GameMode::displayStats()
 {
 }
 
+void GameMode::gameModeRules() 
+{
+	std::cout << "WELCOME TO CURSED WORDLE" << std::endl;
+	std::cout << "There are three game modes:<< std::endl
+		<< "\t 4-letter" << std::endl
+		<< "\t 5-letter" << std::endl
+		<< "\t 6-letter" << std::endl
+	<< "Where each corresponds to the length of the mystery word. Don't worry, there are NO repeated characters AND we don't check 
+		if it exists in the dictionary or not." << std::endl;
+
+	std::cout << "There are two supported gamemodes" << std::endl
+		<< "\t Basic Game Mode" << std::endl
+		<< "\t 3-life Endurance Mode" << std::endl;
+
+	std::cout << "Basic gamemode is your basic wordle activity. You have six times to guess the game's chosen mystery word. There are some
+		hard words and there are some easy ones. Play this gamemode as many times as you want." << std::endl;
+
+	std::cout << "3-life Endurance Mode tests and tracks how many mystery words you can get correct before you lose all three lives. A life is
+		lost only if you cannot guess the mystery word in your six guesses. We will track how many correct words you got." << std::endl;
+
+	std::cout << "GOOD LUCK AND ENJOY THIS SCUFFED VERSION OF WORDLE" << std::endl;
+}
+
+
 void GameMode::runGame()
 {
 	// ALL GAME MODES ARE DEFINED BASED ON THEIR OVERRIDEN FUNCTION. NO CODE HAPPENS HERE
