10fea8ef3897df8b9cf86209e52f93da672ee7da
N Mike <143025594+N5552027@users.noreply.github.com>
2024-04-19 23:56:29 -0700

    Update FiveLM.cpp
    - Added and simplified code for runGame() function.
    - Created runBasicGameMode() to house case 1 option
    - Created Case 3: 3-lives Endurance Mode

--- FiveLM.cpp
+++ FiveLM.cpp
@@ -3,7 +3,8 @@
 void FiveLM::runGame()
 {
 	int playerMenuChoice = 0,
-		vectorIndex = 0;
+		vectorIndex = 0,
+		leaveMenuOption = -1;
 
 	openTXTFile();
 	loadWordBank();
@@ -14,25 +15,45 @@ void FiveLM::runGame()
 	{
 		system("cls");
 		std::cout << "FIVE LETTER GAME MODE:" << std::endl;
+		
+		// DISPLAY PLAYER STATS HERE PERHAPS?
+		
 		std::cin >> playerMenuChoice;
 
 
 		switch (playerMenuChoice) {
 
 		default:
+			std::cin.clear();
+			system("cls");
+			
 			std::cout << "DEFAULT CHOICE" << std::endl;
 			break;
 		case 1:  // PLAY BASIC
+			std::cin.clear();
+			system("cls");
+			
+			do 
+			{
+				system("cls");
+				std::cout << "PLAYING BASIC GAME MODE (IT'S BASICALLY WORDLE)" << std::endl;
+			
+				runBasicGameMode();
 
-			chooseMysteryWord();
-			playerGuess = getPlayerGuess(cols);
-			compareGuessToWord(playerGuess, mysteryWord);
+				std::cout << "\n\n" << std::endl; // SEPERATES THE GAME NEEDED		
 
-			system("pause");
-
+				std::cout << "Would you like to play again? (0 for yes, 1 for no)" << std::endl;
+				std::cin >> leaveMenuOption;
+			
+			} while(leaveMenuOption != 1);
+			
 			break;
-		case 2: // PRINT WORD BANK
-
+		case 2: // PRINT WORD BANK [THIS CASE WILL NOT BE PRESENT IN THE FINAL BUILD]
+			std::cin.clear();
+			system("cls");
+	
+			std::cout << "ALL POSSIBLE WORDS AT PLAY" << std::endl;
+			
 			while (vectorIndex < mysteryWordBankSize) {
 
 				std::cout << mysteryWordBank[vectorIndex] << std::endl;
@@ -43,7 +64,19 @@ void FiveLM::runGame()
 			system("pause");
 
 			break;
+		
+		case 3: // THREE LIVES ENDURANCE MATCH
+
+			
+			
+		break;
+		case 4:
+		
+			std::cout << "EXITING GAMEMODE" << std::endl;
+		
+		break;
 		}
+		
 
 	} while (playerMenuChoice != 4);
 }
@@ -53,8 +86,31 @@ void FiveLM::openTXTFile()
 	this->inFileLoad.open("5LetterWord.txt");
 }
 
-void FiveLM::runBasicGame() {
+void FiveLM::runBasicGameMode() {
 
+bool isPlayerCorrect = false;
+int playerGuesses = 1;
 
+chooseMysteryWord();
+
+	do 
+	{	
+		playerGuess = getPlayerGuess(cols);
+		compareGuessToWord(playerGuess, mysteryWord); // CHANGE TO BOOL OPERATION LATER
+
+		if (isPlayerCorrect == false) {
+			++playerGuesses; // PLAYER GUESS NUMBER CAN BE USED TO CHECK WHICH ROW TO MANAGE/EDIT ON THE VECTOR
+		}
+		
+	} while (playerGuesses < 7 || playerGuess == true) // ESCAPE CONDITION: Player guesses correct word OR player guess exceeds 6
+	
+if (isPlayerCorrect == false) { // PLAYER FAILS TO GUESS THE WORD IN 6 TURNS
+
+	std::cout << "Sorry, you lost!" << std::endl;
+	
+}
+else { // PLAYER MANAGES TO GUESS THE MYSTER WORD IN 6 TURNS OR LESS
+	std::cout << "CONGRATS YOU WON!!" << std::endl
+		<< "*trumpets and fanfare plays in the background*" << std::endl;
 }
 }
