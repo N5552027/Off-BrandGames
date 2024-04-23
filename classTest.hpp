966ea5c4074513f8945bb8f3be715e2beb3e16f7
N Mike <143025594+N5552027@users.noreply.github.com>
2024-04-22 20:26:24 -0700

    Update classTest.hpp

--- classTest.hpp
+++ classTest.hpp
@@ -1,1 +1,20 @@
+#include "Header.hpp"
 
+class testingGrounds {
+public:
+  void runTestingCases();
+
+private:
+
+/* TEST CASE FUNCTIONS */
+  void testLoadWordBank();         // TEST LOADING WORD BANK FUNCTION
+  void testRandomWordSelection();  // TEST RANDOM WORD COMPARISON
+  void testWordComparison();       // 
+  void testColorChange();          //
+  void testWordAddition();         //
+
+/* PRIVATE DATA MEMBERS */
+  std::ifstream inFileStream;
+  std::vector<std::string> wordBank;
+  std::string mysteryWord;
+};
