e60424c874cd08bcea833e5b1c4bc0ad3e901132
Hazopspartan <167109299+Hazopspartan@users.noreply.github.com>
2024-04-23 17:31:46 -0700

    Update GameMode.cpp

--- Sources/GameMode.cpp
+++ Sources/GameMode.cpp
@@ -1,4 +1,4 @@
-#include "Header.hpp"
+#include "GameMode.hpp"
 
 void GameMode::generateBoard(int rows, int cols)
 {
@@ -11,37 +11,22 @@ void GameMode::chooseMysteryWord()
 
 }
 
+bool GameMode::compareGuessToWord(std::vector<Guess>& playerGuess, std::vector<Box>& guessRow, std::string secretWord)
+{
+	return false;
+}
+
 void GameMode::openTXTFile()
 {
 	std::cout << "OPENING FILE STREAM" << std::endl;
 }
 
-/* // BASED ON CONSOLE SCREEN CODING
-std::string GameMode::getPlayerGuess(int wordLength)
-{
-	std::string guess;
-
-	while (1) {
-		std::cout << "Enter a " << wordLength << "-letter word: ";
-		std::cin >> guess;
-		if (guess.length() == wordLength) {
-			break;
-		}
-		else {
-			system("cls");
-		}
-	}
-	return guess;
-}
-*/
-
-
 void GameMode::loadWordBank()
 {
 	while (!this->inFileLoad.eof()) { // END CONDITION: END OF TXT FILE
 		std::string temp;
-		std::getline(inFileLoad, temp); 
-		mysteryWordBank.push_back(temp); 
+		std::getline(inFileLoad, temp);
+		mysteryWordBank.push_back(temp);
 		mysteryWordBankSize++;
 	}
 	std::cout << "WORD BANK LOADED!" << std::endl;
@@ -50,54 +35,61 @@ void GameMode::loadWordBank()
 
 bool compareGuessToWord(std::vector<Guess>& playerGuess, std::vector<Box>& guessRow, std::string secretWord)
 {
-	int guessLetterIndex,
-		secretLetterIndex;
+	int guessLetterIndex, guessBoxIndex;
 
 	bool letterPresence = false;
 
-		for (auto guessIterator = playerGuess.begin(), auto boxIterator = guessRow.begin(), guessLetterIndex = 0; playerGuessIterator != playerGuess.end(); playerGuessIterator++, guessLetterIndex++) { // TRAVERSE THE PLAYER GUESS
-			
-			letterPresence = false;
+	char letter = '\0';
 
-			for (std::string::iterator secretWordIterator = secretWord.begin(), secretLetterIndex = 0; secretWordIterator != secretWord.end(); secretWordIterator++, secretLetterIndex++) { // TRAVERSE THE MYSTERY WORD
+	for (int secretWordIndex = 0, playerGuessIndex = 4; secretWordIndex < 5; secretWordIndex++, playerGuessIndex--) {
 
+		for (int secretWordIndexYellow = 0; secretWordIndexYellow < 5; secretWordIndexYellow++) {
 
-				if (*guess == *secretWordIterator) // LETTER FOUND
-				{
-					letterPresence = true;
+			letter = playerGuess[playerGuessIndex].getstring()[0]; 
 
-					if (guessLetterIndex == secretLetterIndex) {  // INDEX MATCH (CHANGE TO GREEN)
-						guessRow[boxIterator].setGreen();
-					}
-					else {	// INDEXES DON'T MATCH (CHANGE TO YELLOW)
-						guessRow[boxIterator].setYellow;
-					}
-				}
-				
+			std::cout << letter << std::endl;
+			if (letter == secretWord[secretWordIndexYellow]) {
+
+				guessRow[playerGuessIndex].setYellow();
 			}
-
-			if (letterPresence == false) {
-				guessRow[boxIterator].setGray();
-			}
-
-		
 		}
+
+		letter = playerGuess[playerGuessIndex].getstring()[0];
+		if (letter == secretWord[secretWordIndex]) {
+
+			guessRow[playerGuessIndex].setGreen(); 
+
+		}
+	}
+
+	return false;
 }
 
 
-void GameMode::gameModeRules() 
+
+bool GameMode::basicGameMode()
+{
+	// VIRTUAL: DEFINITIONS IN INHERITED CLASSES
+	return false;
+}
+
+void GameMode::ThreeLivesEnduranceMode()
+{
+	// VIRTUAL: DEFINITIONS IN INHERITED CLASSES
+}
+
+void GameMode::gameModeRules()
 {
 	std::cout << "WELCOME TO CURSED WORDLE" << std::endl;
-	std::cout << "There are three game modes:<< std::endl
+	std::cout << "There are three game modes:<< std::endl"
 		<< "\t 4-letter" << std::endl
 		<< "\t 5-letter" << std::endl
 		<< "\t 6-letter" << std::endl
-	<< "Where each corresponds to the length of the mystery word. Don't worry, there are NO repeated characters AND we don't check 
-		if it exists in the dictionary or not." << std::endl;
+		<< "Where each corresponds to the length of the mystery word. Don't worry, there are NO repeated characters AND we don't check if it exists in the dictionary or not." << std::endl;
 
-	std::cout << "There are two supported gamemodes" << std::endl
-		<< "\t Basic Game Mode" << std::endl
-		<< "\t 3-life Endurance Mode" << std::endl;
+			std::cout << "There are two supported gamemodes" << std::endl
+			<< "\t Basic Game Mode" << std::endl
+			<< "\t 3-life Endurance Mode" << std::endl;
 
 	std::cout << "Basic gamemode is your basic wordle activity. You have six times to guess the game's chosen mystery word. There are some hard words and there are some easy ones. Play this gamemode as many times as you want." << std::endl;
 
